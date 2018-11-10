85edd1227daad249688d3b86bc0b7cc5d855148b
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-10 19:18:17 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    p Show.where("rating = ?", self.highest_rating)
+    p Show.where("rating = ?", self.highest_rating)[0]
   end
 end
